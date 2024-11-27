31e4c393fc7982a1c00fdc014feaab74872e19e7
Bar Ben Haim <barbenbh@gmail.com>
2024-11-27 16:48:19 +0200

    healthcheck

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const server = http.createServer(app)
 
 initSocket(server)
 
+//using cron job for preventing the onrender 30 minute delay
+app.get('/api/healthcheck', (req, res) => {
+    res.sendStatus(200)
+})
+
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
